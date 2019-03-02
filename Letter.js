f4844f7f0b4dece49ce444c33d8c5c0f27f9b2e5
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2019-03-01 18:27:28 -0600

    added guess limit of 5

--- Letter.js
+++ Letter.js
@@ -1,17 +1,20 @@
-function Letter(letter){
+function Letter(letter) {
     this.letter = letter;
     this.isGuessed = false;
-    this.check = function(){
-        if(this.isGuessed){
+    this.check = function () {
+        if (this.isGuessed) {
             return this.letter;
         } else {
             return "_";
         }
     }
-    this.guess = function(char){
-        if(char === this.letter){
+    this.guess = function (char) {
+        if (char === this.letter) {
+            console.log("correct");
             this.isGuessed = true;
-        } 
+        } else {
+            console.log("incorrect");
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 var inquirer = require('inquirer');
 var Word = require("./Word");
-
 var wordArr = ["banana", "game", "apple", "computer"];
-var randWord = wordArr[Math.floor(Math.random()*wordArr.length)];
-console.log(randWord)
+var randWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+var guessNum = 4;
+
+console.log(randWord);
+console.log(this.isGuessed); 
 
 var gameWord = new Word();
 gameWord.buildLetters(randWord);
 
-  function play(){
-    inquirer
-  .prompt([
+function play() {
+  inquirer
+    .prompt([
       {
-          type: 'input',
-          name: "guess",
-          message: "guess a letter"
+        type: 'input',
+        name: "guess",
+        message: "guess a letter"
       }
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-    // check if you have any guess left
-    // check if you win or lose
-    // put if to check:
-    console.log(answers);
-    gameWord.guessLetter(answers.guess);
-    gameWord.display();
-    play()
-  });
-  }
+    ])
+    .then(answers => {
+      // check if you have any guess left
+      // check if you win or lose
+      // put if to check:
+      if (guessNum > 0) {
+        // console.log(answers); // answers is an object containing the letters that the user has guessed
+        // for (var i=0; i<randWord.length; i++){
+        //   if (randWord[i] === answers.guess){
+        //     console.log("Correct!")
+        //   } else {
+        //     console.log(`Incorrect! You Have ${guessNum} Guesses Remaining!`);
+        //   }
+        // }
+          // if (!this.isGuessed){
+          //   console.log("Correct!")
+          //   console.log(this.isGuessed); 
+          //   // console.log(Word[0]); 
 
-  play()
+          // } else if (this.isGuessed){
+          //   console.log(`Incorrect! You Have ${guessNum} Guesses Remaining!`);
+          // }
+        gameWord.guessLetter(answers.guess);
+        gameWord.display();
+        play()
+        guessNum--;
+      }
+    });
+}
+
+play()
